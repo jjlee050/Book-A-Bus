617af38146601cd3d23291c266c79f11868ff7b4
jjlee050 <joseph-50@live.com>
2015-04-15 21:36:56 +0800

    Adding Apigee jar

---
+++ wear/src/main/java/com/sg_happenings/book_a_bus/apigee/BookABusApp.java
@@ -0,0 +1,29 @@
+package com.sg_happenings.book_a_bus.apigee;
+
+import android.app.Application;
+
+import com.apigee.sdk.ApigeeClient;
+
+/**
+ * Created by jess on 15-Apr-15.
+ */
+public class BookABusApp extends Application
+{
+
+    private ApigeeClient apigeeClient;
+
+    public BookABusApp()
+    {
+        this.apigeeClient = null;
+    }
+
+    public ApigeeClient getApigeeClient()
+    {
+        return this.apigeeClient;
+    }
+
+    public void setApigeeClient(ApigeeClient apigeeClient)
+    {
+        this.apigeeClient = apigeeClient;
+    }
+}
