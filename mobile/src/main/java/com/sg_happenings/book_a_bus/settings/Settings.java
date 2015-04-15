443d3ab78b4d8ae671a485edd72da689c86b40b6
jjlee050 <joseph-50@live.com>
2015-04-15 21:58:41 +0800

    Adding Settings

---
+++ mobile/src/main/java/com/sg_happenings/book_a_bus/settings/Settings.java
@@ -0,0 +1,9 @@
+package com.sg_happenings.book_a_bus.settings;
+
+/**
+ * Created by jess on 15-Apr-15.
+ */
+public interface Settings {
+    String APIGEE_ORGNAME = "josephlee050";
+    String APIGEE_APPNAME = "book-a-bus";
+}
