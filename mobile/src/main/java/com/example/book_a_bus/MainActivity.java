b9959dc3e3639bae5a16baa03f6f8e25ed354f86
Alfred753 <alfredchan95@hotmail.com>
2015-04-18 13:15:06 +0800

    Bus Service & Bus Arrival API Done. With Codes for current location + getting estimated Arrival times in minutes

--- mobile/src/main/java/com/example/book_a_bus/MainActivity.java
+++ mobile/src/main/java/com/example/book_a_bus/MainActivity.java
@@ -1,25 +1,40 @@
 package com.example.book_a_bus;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.book_a_bus.listeners.TaskListener;
 import com.example.book_a_bus.objectmodel.BusArrivalInfo;
 import com.example.book_a_bus.objectmodel.SBSInfo;
 import com.example.book_a_bus.tasks.BusArrivalTask;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
-public class MainActivity extends Activity implements TaskListener {
+public class MainActivity extends Activity implements TaskListener, LocationListener {
 
 	/*
 	 * This is for BUS ROUTES ・SBST BUS SERVICE INFO API
 	 */
 
     TextView test;
+    private LocationManager locationManager;
+    private String provider;
+    private String currentLat, currentLon;
 
     ArrayList<SBSInfo> sbsInfoList = new ArrayList<SBSInfo>();
 
@@ -32,12 +47,71 @@ public class MainActivity extends Activity implements TaskListener {
 
         test = (TextView) findViewById(R.id.test);
 
-        //To Start the task
-
+        //To Start the task for Bus Services
         //BusServiceInfoTask s = new BusServiceInfoTask(MainActivity.this);
       // s.executeGetSBSInfo("SBST");
+
+
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+
+        } else {
+            currentLat = "Location not available";
+            currentLon = "Location not available";
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(provider, 400, 5, this); // provider , minTime, minDistance, listener
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double lat = (double) (location.getLatitude());
+        double lng = (double) (location.getLongitude());
+        currentLat = String.valueOf(lat);
+        currentLon = String.valueOf(lng);
+        //To Start the task for Bus Arrival
         BusArrivalTask bat = new BusArrivalTask(MainActivity.this);
-        bat.executeGetArrivalTimeInfo("1.379820", "103.879626", "500");
+        bat.executeGetArrivalTimeInfo(currentLat, currentLon, "500"); //500 is the distance in metres
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -113,25 +187,92 @@ public class MainActivity extends Activity implements TaskListener {
                     + "\nBus Desc: " + busStopList.get(i).getBusStopDesc() + "\n\n";
         }*/
 
+
+
         for(int i = 0; i < busArrList.size(); i++)
         {
-            temp += "Bus Stop No: " + busArrList.get(i).getBusStopNo()
-                    + "\nBus Desc: " + busArrList.get(i).getBusStopDesc()
-                    + "\nLat: " + busArrList.get(i).getLat()
-                    + "\nLon: " + busArrList.get(i).getLon()
-                    + "\nServiceNo: " + busArrList.get(i).getServiceNo()
-                    + "\nStatus: " + busArrList.get(i).getStatus()
-                    + "\nEstimated Arr: " + busArrList.get(i).getEstimated_Arr()
-                    + "\nLoad: " + busArrList.get(i).getLoad()
-                    + "\nFeature: " + busArrList.get(i).getFeature()
-                    + "\nEstimated ArrS: " + busArrList.get(i).getEstimated_ArrS()
-                    + "\nLoadS: " + busArrList.get(i).getLoadS()
-                    + "\nFeatureS: " + busArrList.get(i).getFeatureS() + "\n\n";
+            String dateArrTime = "";
+            String timeArrTime = "";
+            String dateSArrTime = "";
+            String timeSArrTime = "";
+            Date date = null;
+            Date dateS = null;
+            Date todayDate = null;
+
+            //To get the date & Time from the API
+            String estimatedArrList[] = busArrList.get(i).getEstimated_Arr().split("T");
+            if(estimatedArrList.length == 2) {
+                dateArrTime = estimatedArrList[0];
+                timeArrTime = estimatedArrList[1];
+            }
+            String estimatedArrSList[] = busArrList.get(i).getEstimated_ArrS().split("T");
+            if(estimatedArrSList.length == 2) {
+                dateSArrTime = estimatedArrSList[0];
+                timeSArrTime = estimatedArrSList[1];
+            }
+            //Set Date Format for GMT +0
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+            //Set Date Format for GMT + 8
+            DateFormat converter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            converter.setTimeZone(TimeZone.getTimeZone("Asia/Singapore"));
+
+            //Set Date Format for Local Time
+            Calendar c = Calendar.getInstance();
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String localTime = df.format(c.getTime());
+
+            int minutes = 0;
+            int minutesS = 0;
+            try {
+                date =  formatter.parse(dateArrTime + " " + timeArrTime); //Still in GMT+0
+                dateS =  formatter.parse(dateSArrTime + " " + timeSArrTime); //Still in GMT+0
+                todayDate = converter.parse(localTime); //In GMT+8
+
+                String convertedDate = converter.format(date); //In GMT+8
+                String convertedDateS = converter.format(dateS); //In GMT+8
+                date = formatter.parse(convertedDate); //In GMT+8
+                dateS = formatter.parse(convertedDateS); //In GMT+8
+
+                //get Time in milli seconds
+                long todayTime_ms = todayDate.getTime();
+                long Sub_bus_dateS_ms = dateS.getTime();
+                long next_bus_date_ms = date.getTime();
+                long diff = next_bus_date_ms - todayTime_ms;
+                long diff1 = Sub_bus_dateS_ms - todayTime_ms;
+                //Get the Minutes difference
+                //minutes - For Next Bus
+                //minutes - For Subsequent Bus
+                minutes = (int) diff / (60 * 1000) % 60;
+                minutesS = (int) diff1 / (60 * 1000) % 60;
+
+                temp += "Bus Stop No: " + busArrList.get(i).getBusStopNo()
+                        + "\nBus Desc: " + busArrList.get(i).getBusStopDesc()
+                        + "\nLat: " + busArrList.get(i).getLat()
+                        + "\nLon: " + busArrList.get(i).getLon()
+                        + "\nServiceNo: " + busArrList.get(i).getServiceNo()
+                        + "\nStatus: " + busArrList.get(i).getStatus()
+                        + "\nEstimated Arr in Date: " + convertedDate
+                        + "\nLoad: " + busArrList.get(i).getLoad()
+                        + "\nFeature: " + busArrList.get(i).getFeature()
+                        + "\nEstimated ArrS in Date: " + convertedDateS
+                        + "\nLoadS: " + busArrList.get(i).getLoadS()
+                        + "\nFeatureS: " + busArrList.get(i).getFeatureS() //"" or "WAB" //WAB == With Wheelchair enabled bus
+                        + "\nEstimateTime: " + minutes
+                        + "\nEstimateTimeS: " + minutesS + "\n\n";
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+
+
+
+
+
 
         }
 
-        //Bus Arrival Time show GMT +0 Timing instead of GMT +8
-        //API EstimatedArrival – Singapore Standard Time = Ariival Time
 
         test.setText(temp);
     }
